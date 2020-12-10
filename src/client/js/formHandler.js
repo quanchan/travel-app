97b7b61e240ef2023c3a4ffaa4b1789462ca2deb
quantran <quan.trananh12@gmail.com>
2020-12-10 14:28:33 +0700

    Disabled button

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,11 +2,14 @@ const handleSubmit = async(event) => {
     event.preventDefault()
     // check what text was put into the form field
     let formText = document.getElementById('name').value
+    let submitButton = document.getElementById('submit-button')
     formText = {
         txt: formText
     }
     Client.checkForName(formText)
     console.log("::: Form Submitted :::")
+    submitButton.disabled = true
+
     try {
         let response = await fetch('http://localhost:8080/process-text', {
             method: 'POST',
@@ -21,6 +24,7 @@ const handleSubmit = async(event) => {
         response = await response.json()
         console.log('This is the response we received: ', response)
         document.getElementById('results').innerHTML = JSON.stringify(response)
+        submitButton.disabled = false
     } catch(err) {
         console.log("Something is wrong in handleSubmit: ", err)
     }
