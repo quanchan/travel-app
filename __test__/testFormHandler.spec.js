a70a913ca46bd8029ca08cb3f1b8aa335ae88c18
quantran <quan.trananh12@gmail.com>
2020-12-10 16:18:56 +0700

    setting up travel-app

--- __test__/testFormHandler.spec.js
+++
@@ -1,8 +0,0 @@
-import { handleSubmit } from "../src/client/js/formHandler"
-
-describe("Testing the submit functionality", () => {
-    test("Testing the handleSubmit() function", async () => {
-        const data = await expect(handleSubmit())
-        expect(data).toBe(true)
-    }) 
-})
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,39 +1,4 @@
 const handleSubmit = async(event) => {
     event.preventDefault()
-    let formText = document.getElementById('name').value
-    let submitButton = document.getElementById('submit-button')
-    formText = {
-        txt: formText
-    }
-    // check whether the text area is empty
-
-    if (!Client.isValidText(formText.txt)) {
-        return
-    }
-    
-    console.log("::: Form Submitted :::")
-    submitButton.disabled = true
-
-    try {
-        let response = await fetch('http://localhost:8080/process-text', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formText)
-        })
-        console.log("POST response", response)
-        response = await fetch('http://localhost:8080/processed-text')
-        response = await response.json()
-        console.log('This is the response we received: ', response)
-        document.getElementById('results').innerHTML = JSON.stringify(response)
-        submitButton.disabled = false
-        return true
-    } catch(err) {
-        console.log("Something is wrong in handleSubmit: ", err)
-        return false
-    }
 }
-
 export { handleSubmit }
